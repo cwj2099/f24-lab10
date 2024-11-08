d89acb680c1aecc3b32ff44553d02ba97af58b88
cwj2099 <wc1629@nyu.edu>
2024-11-07 21:32:01 -0500

    Decouple core logic from ui

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,50 +1,43 @@
 import React, { useState } from 'react'
 import './Quiz.css'
 import QuizQuestion from '../core/QuizQuestion';
+import QuizCore from '../core/QuizCore';
 // Hint: Take advantage of the QuizQuestion interface
 
 interface QuizState {
-  questions: QuizQuestion[]
-  currentQuestionIndex: number
   selectedAnswer: string | null
-  score: number
 }
 
 const Quiz: React.FC = () => {
   // TODO: Task1 - Seprate the logic of quiz from the UI.
   // Hint: Take advantage of QuizCore to manage quiz state separately from the UI.
-  const initialQuestions: QuizQuestion[] = [
-    {
-      question: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Paris', 'Madrid'],
-      correctAnswer: 'Paris',
-    },
-  ];
+  
+  
   const [state, setState] = useState<QuizState>({
-    questions: initialQuestions,
-    currentQuestionIndex: 0,  // Initialize the current question index.
     selectedAnswer: null,  // Initialize the selected answer.
-    score: 0,  // Initialize the score.
   });
+  
 
   const handleOptionSelect = (option: string): void => {
     setState((prevState) => ({ ...prevState, selectedAnswer: option }));
+    //selectedAnswer = option;
+    console.log("omg");
   }
-
+  const core: QuizCore = new QuizCore();
 
   const handleButtonClick = (): void => {
     // TODO: Task3 - Implement the logic for button click ("Next Question" and "Submit").
     // Hint: You might want to check for a function in the core logic to help with this.
   } 
 
-  const { questions, currentQuestionIndex, selectedAnswer, score } = state;
-  const currentQuestion = questions[currentQuestionIndex];
+  const { selectedAnswer} = state;
+  const currentQuestion = core.getCurrentQuestion();
 
   if (!currentQuestion) {
     return (
       <div>
         <h2>Quiz Completed</h2>
-        <p>Final Score: {score} out of {questions.length}</p>
+        <p>Final Score: {core.getScore()} out of {core.getTotalQuestions()}</p>
       </div>
     );
   }
